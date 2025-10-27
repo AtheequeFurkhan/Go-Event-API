7ecaffd55b6bf3b6ab9a0715892ed4ad211d7210
Atheeque <atheequemf2003@gmail.com>
2025-10-27 23:59:09 +0530

    Add basic logis

--- migrate/migrations/main.go
+++ migrate/migrations/main.go
@@ -4,6 +4,11 @@ import (
 	"database/sql"
 	"log"
 	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -20,4 +25,27 @@ func main() {
 	defer db.Close()
 	
 	instance, err := sqlite3.WithInstance(db,&sqlite3.Config{})
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fSrc, err := (&file.File{}).Open("./cmd/migrate/migrations")
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	mig, err := migrate.NewWithInstance("file",fSrc,"sqlite3",instance)
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	switch direction{
+	case "up":
+		if err := mig.Up();
+		err!= nil && err!= migrate.ErrNoChange{
+			log.Fatal(err)
+		}
+	}
 }
