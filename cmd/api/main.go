476d448dd3ba7ff66d75680e0499e717e1343aa9
Atheeque <atheequemf2003@gmail.com>
2025-10-31 23:43:58 +0530

    Refactor main function to encapsulate application configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"go-event-api/internal/database"
+	"go-event-api/internal/env"
 	"log"
 )
 
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
+
 func main() {
-	db, err := sql.Open("sqlite3",	"./data/db.sqlite3")
+	db, err := sql.Open("sqlite3", "./data/db.sqlite3")
 
 	if err != nil {
 		log.Fatal(err)
@@ -14,5 +22,15 @@ func main() {
 
 	defer db.Close()
 
+	models := database.NewModels(db)
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "XXXXXXXXXXX"),
+		models:    models,
+	}
+
+	if err := app.server(); err != nil {
+		log.Fatal(err)
+	}
 
 }
