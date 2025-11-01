e50334656d5695ad0b90a4c333708559588a2ddc
Atheeque <atheequemf2003@gmail.com>
2025-11-01 23:21:07 +0530

    Implement createEvent handler for event creation

---
+++ cmd/api/events.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"go-event-api/internal/database"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) createEvent(c *gin.Context) {
+	var event database.Events
+
+	if err := c.ShouldBindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := app.models.Events.insert(&event)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, event)
+
+}
