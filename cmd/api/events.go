7edca93968707656dd0fbd012754625b46bb4400
Atheeque <atheequemf2003@gmail.com>
2025-11-02 23:53:36 +0530

    Implement updateEvent handler for updating event

--- cmd/api/events.go
+++ cmd/api/events.go
@@ -47,3 +47,22 @@ func (app *application) getEvent(c *gin.Context) {
 
 	c.JSON(http.StatusOK, event)
 }
+
+func (app *application) updateEvent(c *gin.Context) {
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	event, err := app.models.Events.Get(id)
+
+	if event == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event Not Found"})
+	}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch the Event"})
+	}
+
+	c.JSON(http.StatusOK, event)
+}
