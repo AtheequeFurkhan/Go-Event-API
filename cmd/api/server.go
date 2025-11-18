d894d90fa1398afc9534eff5b7d3d88e9c4d74a7
Atheeque <atheequemf2003@gmail.com>
2025-11-18 14:17:44 +0530

    Remove unnecessary blank line in server.go

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -15,7 +15,6 @@ func (app *application) server() error {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
-
 	log.Printf("Starting Server at %d", app.port)
 	return server.ListenAndServe()
 }
